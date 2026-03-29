c4b04977a1f2c6ea0bfb111e0210600f088b22b6
gowthamanniit <karurgowthaman@gmail.com>
2026-03-29 08:23:55 +0530

    download export excel

--- src/Cart.js
+++ src/Cart.js
@@ -1,11 +1,31 @@
 import React from 'react'
 import { useCart } from 'react-use-cart'
-
+import { useState } from 'react'
+import axios from 'axios'
 const Cart = () => {
     const {isEmpty,totalUniqueItems,items,totalItems,cartTotal,updateItemQuantity,removeItem,emptyCart} = useCart()
-
+    const [cname,setCname]=useState("")
     if (isEmpty) return <h1>The Cart is Empty</h1>
-  return (
+    
+  const insertItem=async()=>{
+    const iteminput={
+        name:cname,
+        orders:items
+    }
+    await axios.post("http://localhost:8099/api/products",iteminput).then((res)=>{
+        console.log(res.data)
+
+        alert("products saved successfully")             
+        
+        }).catch((err)=>{
+            if(err.response){
+              console.log(err.response.data)
+            }
+        })
+
+
+  }
+    return (
     <div className='row justify-content-center'>
         <h5>Cart:{totalUniqueItems} Total Items :{totalItems} </h5>
         <table className='table table-light table-hover m-0'>
@@ -32,8 +52,9 @@ const Cart = () => {
         </tbody>
         </table>
         <h2 className='text-end bg-success me-2'>Total price : ₹ {cartTotal}   </h2>
-        <button className='btn btn-primary col-2'  onClick={emptyCart}>CLear Cart</button>
-        <button className='btn btn-warning col-2'>Buy Now</button>
+        Customer Name:<input style={{width:"200px"}}  type="text" id="cname" value={cname} onChange={(e)=>setCname(e.target.value)}></input>
+        <button className='btn btn-primary col-2'  onClick={emptyCart}>Clear Cart</button>
+        <button className='btn btn-warning col-2' onClick={()=>insertItem()}>Sale</button>
     </div>
 
    )
--- src/Home.js
+++ src/Home.js
@@ -1,13 +1,64 @@
 import React from 'react'
 import ItemCard from './ItemCard'
 import data from './data'
+import axios from 'axios'
+import * as XLSX from 'xlsx';
+import { saveAs } from 'file-saver';
 const Home = () => {
   const logoutfun=()=>{
     window.location.href="http://localhost:3000"
   }
+  const flattenData = (data) => {
+    return data.map(item => ({
+      //Date: item.date,
+      // Map nested properties to flat fields
+      //"Number Card": item.num_card.number || '', 
+      //"Number Check": item.num_check.number || '' 
+      name:item.name,
+        itemname:item.orders.map(i=>i.itemname)
+    
+    
+    }));
+  };
+  const exportdata=async()=>{
+    await axios.get("http://localhost:8099/api/products").then((res)=>{
+      console.log(res.data)
+      const ws = XLSX.utils.json_to_sheet(flattenData(res.data));
+      const wb = { Sheets: { 'Data': ws }, SheetNames: ['Data'] };
+      const excelBuffer = XLSX.write(wb, { bookType: 'xlsx', type: 'array' });
+      const blob = new Blob([excelBuffer], { type: 'application/octet-stream' });
+      saveAs(blob, "data.xlsx"); 
+/*
+      // Convert JSON to worksheet
+  const worksheet = XLSX.utils.json_to_sheet(res.data);
+
+  // Create workbook
+  const workbook = XLSX.utils.book_new();
+  XLSX.utils.book_append_sheet(workbook, worksheet, "salessheet");
+
+  // Generate Excel file buffer
+  const excelBuffer = XLSX.write(workbook, {
+    bookType: "xlsx",
+    type: "array"
+  });
+
+  // Save file
+  const fileData = new Blob([excelBuffer], {
+    type: "application/octet-stream"
+  });
+
+  saveAs(fileData, "salesdata.xlsx");
+
+  */
+      }).catch((err)=>{
+          if(err.response){
+            console.log(err.response.data)
+           }
+      })
+  }
   return (
     <>
-    <h1 className='text-center bg-info mt-5 mb-5'>Shopping Cart - Created By K.Gowthaman M.C.A.,<button onClick={logoutfun}>➜]</button></h1>
+    <h1 className='text-center bg-info mt-5 mb-5'>Shopping Cart - Created By K.Gowthaman M.C.A.,<button onClick={logoutfun}>➜]</button><button onClick={exportdata}>Export Excel</button></h1>
 
     <div className='row justify-content-center'>
     {
